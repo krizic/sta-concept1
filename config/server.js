da4bd531fab4fa875d3db6da6537839d0a04b094
Vedran Krizic <vedran.krizic@prodyna.com>
2021-10-22 16:46:50 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8103446fcef3533b9ed171e88a41832d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '738ea0a0-2a49-496d-9922-219e2e972aa1'
+};
